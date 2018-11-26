a67a3891af2aaa38d13c2ee16c8d772798fccd7b
Piotr Janczyk <pjanczyk97@gmail.com>
2018-11-26 20:47:04 +0100

    Move constants from Board to BoardFactory

--- src/main/java/pl/edu/agh/torbjorns/board/Board.java
+++ src/main/java/pl/edu/agh/torbjorns/board/Board.java
@@ -2,26 +2,13 @@ package pl.edu.agh.torbjorns.board;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import pl.edu.agh.torbjorns.card.Suit;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@Getter
 public class Board {
-
-    public static final int WORKING_CARD_STACK_COUNT = 8;
-    public static final Suit[] FINISHED_CARD_STACK_SUITS = {
-            Suit.HEARTS, Suit.DIAMONDS, Suit.SPADES, Suit.CLUBS,
-            Suit.HEARTS, Suit.DIAMONDS, Suit.SPADES, Suit.CLUBS
-    };
-
-    @Getter
     private final List<CardStack> finishedCardStacks;
-
-    @Getter
     private final List<CardStack> workingCardStacks;
-
-    @Getter
     private final BufferZone bufferZone;
-
 }
--- src/main/java/pl/edu/agh/torbjorns/board/BoardFactory.java
+++ src/main/java/pl/edu/agh/torbjorns/board/BoardFactory.java
@@ -2,6 +2,7 @@ package pl.edu.agh.torbjorns.board;
 
 import lombok.RequiredArgsConstructor;
 import pl.edu.agh.torbjorns.board.deck.Deck;
+import pl.edu.agh.torbjorns.card.Suit;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -11,17 +12,23 @@ import static java.util.stream.Collectors.*;
 @RequiredArgsConstructor
 public class BoardFactory {
 
+    private static final int WORKING_CARD_STACK_COUNT = 8;
+    private static final Suit[] FINISHED_CARD_STACK_SUITS = {
+            Suit.HEARTS, Suit.DIAMONDS, Suit.SPADES, Suit.CLUBS,
+            Suit.HEARTS, Suit.DIAMONDS, Suit.SPADES, Suit.CLUBS
+    };
+
     private final Dealer dealer;
 
     public Board createBoard(Deck deck) {
         List<CardStack> finishedCardStacks =
-                Stream.of(Board.FINISHED_CARD_STACK_SUITS)
+                Stream.of(FINISHED_CARD_STACK_SUITS)
                         .map(FinishedCardStack::new)
                         .collect(toUnmodifiableList());
 
         List<CardStack> workingCardStacks =
                 Stream.generate(WorkingCardStack::new)
-                        .limit(Board.WORKING_CARD_STACK_COUNT)
+                        .limit(WORKING_CARD_STACK_COUNT)
                         .collect(toUnmodifiableList());
 
         var bufferZone = new BufferZone();
