4e33d58cd456c2caf93272cc38f8e1d736c445bd
Piotr Janczyk <pjanczyk97@gmail.com>
2018-11-26 20:44:38 +0100

    Add Dealer class

--- src/main/java/pl/edu/agh/torbjorns/board/BoardFactory.java
+++ src/main/java/pl/edu/agh/torbjorns/board/BoardFactory.java
@@ -2,17 +2,16 @@ package pl.edu.agh.torbjorns.board;
 
 import lombok.RequiredArgsConstructor;
 import pl.edu.agh.torbjorns.board.deck.Deck;
-import pl.edu.agh.torbjorns.card.Card;
-import pl.edu.agh.torbjorns.card.Rank;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
 
+@RequiredArgsConstructor
 public class BoardFactory {
+
+    private final Dealer dealer;
 
     public Board createBoard(Deck deck) {
         List<CardStack> finishedCardStacks =
@@ -29,47 +28,9 @@ public class BoardFactory {
 
         var board = new Board(finishedCardStacks, workingCardStacks, bufferZone);
 
-        new Placer(board, deck)
-                .placeCards();
+        dealer.dealCards(board, deck);
 
         return board;
-    }
-
-    @RequiredArgsConstructor
-    private class Placer {
-        private final Board board;
-        private final Deck deck;
-
-        private int currentStackIndex = 0;
-        private Deque<Card> currentStack = new ArrayDeque<>();
-
-        public void placeCards() {
-            var firstKingPut = false;
-
-            while (!deck.isEmpty()) {
-                var card = deck.takeCard();
-
-                if (card.getRank() == Rank.KING) {
-                    if (!firstKingPut) {
-                        currentStack.addFirst(card);
-                        firstKingPut = true;
-                    } else {
-                        completeCurrentStack();
-                        currentStack.addLast(card);
-                    }
-                } else {
-                    currentStack.addLast(card);
-                }
-            }
-
-            completeCurrentStack();
-        }
-
-        public void completeCurrentStack() {
-            board.getWorkingCardStacks().get(currentStackIndex).setCards(currentStack);
-            currentStackIndex++;
-            currentStack = new ArrayDeque<>();
-        }
     }
 
 }
